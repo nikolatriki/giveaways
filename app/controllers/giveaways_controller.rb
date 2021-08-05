5a66be49670c0d50f5dc2aa56f8bf6c6f1a82398
nikolatriki <kjofte@yahoo.com>
2021-08-06 00:50:11 +0200

    Create view with form for new giveaway, routs and action

--- app/controllers/giveaways_controller.rb
+++ app/controllers/giveaways_controller.rb
@@ -11,11 +11,26 @@ class GiveawaysController < ApplicationController
   end
 
   def create
+    giveaway = Giveaway.new(giveaway_params)
+    giveaway.save
+
+    redirect_to giveaway_path(giveaway)
+    # render plain: params[:giveaway].inspect -for checking the parametars hash created
   end
 
   def update
   end
 
   def destroy
+  end
+
+  private
+
+  def giveaway_params
+    params.require(:giveaway).permit(:title, :description, :location)
+    # I found out that it can be written as:
+    # params[:giveaway].permit(:title, :description, :location)
+    # :giveaway is the key from the params hash created with 'form_with' helper using 'scope:', or 'model:' methods
+    # If you don't have scoped parametars, there is no need for require method in strong params altogether.
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   get '/giveaways', to: 'giveaways#index'
+  get 'giveaways/new', to: 'giveaways#new', as: :new_giveaway
   get 'giveaways/:id', to: 'giveaways#show', as: :giveaway
+  post '/giveaways', to: 'giveaways#create'
 
   root 'giveaways#index'
 end
