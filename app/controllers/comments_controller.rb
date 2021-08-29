9308c8f1bcb9dda3e1871fbf45c4f72d73f25b41
nikolatriki <kjofte@yahoo.com>
2021-08-29 22:04:44 +0200

    Generate comments controller & defined new and create actions

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,20 @@
+class CommentsController < ApplicationController
+  def new
+    @giveaway = Giveaway.find(params[:giveaway_id])
+    @comment = @giveaway.comments.build
+  end
+
+  def create
+    giveaway = Giveaway.find(params[:giveaway_id])
+    comment = giveaway.comments.build(comment_params)
+
+    comment.save
+    redirect_to giveaway
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:commenter, :body)
+  end
+end
