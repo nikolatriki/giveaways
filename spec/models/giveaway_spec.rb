9784a996e64c6e074e5af7f7b19d64d13ad6bc68
nikolatriki <kjofte@yahoo.com>
2021-09-01 23:23:50 +0200

    Model Giveaway assocition and dependency tests with created comments factory

--- spec/models/giveaway_spec.rb
+++ spec/models/giveaway_spec.rb
@@ -3,11 +3,24 @@
 require 'rails_helper'
 
 RSpec.describe Giveaway, type: :model do
-  subject(:givey) { create(:giveaway) }
+  subject(:giveaway) { create(:giveaway) }
 
   describe 'validations' do
-    it { expect(givey).to validate_presence_of(:title) }
-    it { expect(givey).to validate_presence_of(:description) }
-    it { expect(givey).to validate_presence_of(:location) }
+    it { expect(giveaway).to validate_presence_of(:title) }
+    it { expect(giveaway).to validate_presence_of(:description) }
+    it { expect(giveaway).to validate_presence_of(:location) }
+  end
+
+  describe 'associations' do
+    it { expect(giveaway).to have_many(:comments) }
+  end
+
+  describe 'dependency' do
+    it { expect(giveaway).to have_many(:comments).dependent(:destroy) }
+
+    it 'destroys comments' do
+      create_list(:comment, 1, giveaway: giveaway)
+      expect { giveaway.destroy }.to change(Comment, :count).by(-1)
+    end
   end
 end
