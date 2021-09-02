923832e4d747cdabb8218c66eef712433dfaacd9
nikolatriki <kjofte@yahoo.com>
2021-09-03 00:09:16 +0200

    Comments edit/update actions, form partial and link to form

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -17,9 +17,28 @@ class CommentsController < ApplicationController
     end
   end
 
+  def edit
+    find_comment
+  end
+
+  def update
+    find_comment
+
+    if @comment.update(comment_params)
+      redirect_to @giveaway
+    else
+      render :edit
+    end
+  end
+
   private
 
   def comment_params
     params.require(:comment).permit(:commenter, :body)
+  end
+
+  def find_comment
+    @giveaway = Giveaway.find(params[:giveaway_id])
+    @comment = Comment.find(params[:id])
   end
 end
