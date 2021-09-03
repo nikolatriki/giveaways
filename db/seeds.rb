48ece50a0356a3156bb71a58452dcc180dda1f9f
nikolatriki <kjofte@yahoo.com>
2021-09-03 23:57:00 +0200

    Create seed file for populating the database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+  30.times do
+    Giveaway.create(
+    title: Faker::Commerce.product_name,
+    description: Faker::Lorem.paragraphs,
+    location: Faker::Address.city
+    )
+  end
+
+  Giveaway.all.each do |givey|
+    2.times do
+      givey.comments.create(
+        commenter: Faker::Movies::Lebowski.character,
+        body: Faker::Movies::Lebowski.quote
+      )
+    end
+  end
+
+ puts 'Done!'
