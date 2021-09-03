de057856adcd6652bf38285c6af161ea60aaca3f
nikolatriki <kjofte@yahoo.com>
2021-09-03 09:32:37 +0200

    Create Comment Model spec

---
+++ spec/models/comment_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+RSpec.describe Comment, type: :model do
+  subject(:comment) { create(:comment) }
+
+  describe 'validation' do
+    it { expect(comment).to validate_presence_of(:commenter) }
+    it { expect(comment).to validate_presence_of(:body) }
+  end
+
+  describe 'assosiations' do
+    it { expect(comment).to belong_to(:giveaway) }
+  end
+end
