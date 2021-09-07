2d86bbb421023947cb40acf9ebe5e3119d03b1b7
nikolatriki <kjofte@yahoo.com>
2021-09-08 01:34:27 +0200

    Run migration add_index_to_users_email

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_26_132427) do
+ActiveRecord::Schema.define(version: 2021_09_07_214806) do
 
   create_table "comments", force: :cascade do |t|
     t.string "commenter"
@@ -27,6 +27,14 @@ ActiveRecord::Schema.define(version: 2021_08_26_132427) do
     t.string "location"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
   add_foreign_key "comments", "giveaways"
