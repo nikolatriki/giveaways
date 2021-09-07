3af8b0f0ecbc1bd8c45ef0529e747a0081d84e7e
nikolatriki <kjofte@yahoo.com>
2021-09-08 01:35:17 +0200

    Added validation to model User

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,18 @@
+# frozen_string_literal: true
+
 class User < ApplicationRecord
+  VALID_REGEX_EMAIL = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+
+  before_save :email_to_downcase
+
+  validates :name, presence: true, length: { maximum: 50 }
+  validates :email, presence: true, length: { maximum: 250 },
+                    format: { with: VALID_REGEX_EMAIL },
+                    uniqueness: true
+
+  private
+
+  def email_to_downcase
+    self.email = email.downcase
+  end
 end
