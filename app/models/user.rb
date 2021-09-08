78d98b89909d41f120d9058563d61b9e8dd22c89
nikolatriki <kjofte@yahoo.com>
2021-09-08 17:58:52 +0200

    Added Bcrypt password_digest to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,14 +1,20 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
+  MAXIMUM_NAME_LENGTH = 50
+  MAXIMUM_EMAIL_LENGTH = 250
   VALID_REGEX_EMAIL = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+  MINIMUM_PASSWORD_LENGTH = 6
 
   before_save :email_to_downcase
 
-  validates :name, presence: true, length: { maximum: 50 }
-  validates :email, presence: true, length: { maximum: 250 },
+  has_secure_password
+
+  validates :name, presence: true, length: { maximum: MAXIMUM_NAME_LENGTH }
+  validates :email, presence: true, length: { maximum: MAXIMUM_EMAIL_LENGTH },
                     format: { with: VALID_REGEX_EMAIL },
                     uniqueness: true
+  validates :password, length: { minimum: MINIMUM_PASSWORD_LENGTH }
 
   private
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_07_214806) do
+ActiveRecord::Schema.define(version: 2021_09_08_113754) do
 
   create_table "comments", force: :cascade do |t|
     t.string "commenter"
@@ -34,6 +34,7 @@ ActiveRecord::Schema.define(version: 2021_09_07_214806) do
     t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "password_digest"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
