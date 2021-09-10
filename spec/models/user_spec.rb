03c96975aed1c0929048f6d8f6aca972cc59c0d2
nikolatriki <kjofte@yahoo.com>
2021-09-10 14:26:24 +0200

    Refactor User Model spec for greater readability

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,36 +3,33 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  let!(:user) { create(:user) }
+  subject(:user) { create(:user) }
 
-  context 'when saving the User' do
-    it 'transformes email to donwcase' do
-      new_user = create(:user, email: 'TEST@TEST.COM')
-
-      expect(new_user.email).to eq('test@test.com')
-    end
-  end
-
-  context 'when using invalid email format' do
-    it 'is invalid' do
-      john = build(:user, email: 'test@invalid')
-      expect(john.valid?).to be false
-    end
-  end
-
-  describe 'validations' do
+  context 'with validations' do
     it { expect(user).to validate_presence_of(:name) }
     it { expect(user).to validate_presence_of(:email) }
     it { expect(user).to validate_presence_of(:password).ignoring_interference_by_writer }
-
-    it { expect(user).to have_secure_password }
-  end
-
-  context 'when matching uniqueness of email' do
     it { expect(user).to validate_uniqueness_of(:email).case_insensitive }
+    it { expect(user).to have_secure_password }
+    it { expect(user).to validate_length_of(:password).is_at_least(User::MINIMUM_PASSWORD_LENGTH) }
+    it { expect(user).to validate_length_of(:name).is_at_most(User::MAXIMUM_NAME_LENGTH) }
+    it { expect(user).to validate_length_of(:email).is_at_most(User::MAXIMUM_EMAIL_LENGTH) }
   end
 
-  it { expect(user).to validate_length_of(:password).is_at_least(User::MINIMUM_PASSWORD_LENGTH) }
-  it { expect(user).to validate_length_of(:name).is_at_most(User::MAXIMUM_NAME_LENGTH) }
-  it { expect(user).to validate_length_of(:email).is_at_most(User::MAXIMUM_EMAIL_LENGTH) }
+  context 'with email downcase' do
+    let(:valid_email) { 'TEST@TEST.COM' }
+    let(:valid_user) { create(:user, email: valid_email) }
+
+    it 'transforme email to downcase' do
+      expect(valid_user.email).to eq(valid_email.downcase)
+    end
+  end
+
+  context 'when invalid email format' do
+    let(:invalid_user) { build(:user, email: 'test@invalid') }
+
+    it 'is invalid' do
+      expect(invalid_user.valid?).to be false
+    end
+  end
 end
