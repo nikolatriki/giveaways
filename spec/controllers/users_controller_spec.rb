47a2398748cc82bdc66c55029c7c98c49a6630d7
nikolatriki <kjofte@yahoo.com>
2021-09-11 10:37:19 +0200

    Rubocop fix

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -16,11 +16,8 @@ RSpec.describe UsersController, type: :controller do
     context 'with valid attributes' do
       it 'creates new user' do
         expect do
-          post :create,
-               params: {
-                 user: { name: 'Any Name', email: 'test@test.com', password: '123456',
-                         password_confirmation: '123456' }
-               }
+          post :create, params: { user: { name: 'Any Name', email: 'test@test.com', password: '123456',
+                                          password_confirmation: '123456' } }
         end.to change(User, :count).by(1)
       end
     end
