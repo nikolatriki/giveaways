2501c0396fff2aac88362f07808f1571d96f6ee2
nikolatriki <kjofte@yahoo.com>
2021-10-16 12:12:47 +0200

    Validate content type images

--- app/models/giveaway.rb
+++ app/models/giveaway.rb
@@ -6,4 +6,14 @@ class Giveaway < ApplicationRecord
   has_many_attached :images, dependent: :destroy
 
   validates :title, :description, :location, presence: true, length: { minimum: 5 }
+  validate :image_validation
+
+  private
+
+  def image_validation
+    if images.attached? && !images.content_type.in?(%w[images/jpeg images/jpg images/png])
+      images.purge # delete the uploaded file
+      errors.add(:images, 'Must be a jpeg, jpg or png image file')
+    end
+  end
 end
