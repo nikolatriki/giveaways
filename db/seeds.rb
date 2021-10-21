09f7ffda3a44b8159a8d148e6c22de7c1326abd6
orcemaceski <orce@maceski.net>
2021-10-21 16:13:10 +0200

    Added images to seed and their display in index

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,5 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+puts "Seeding started..."
+
 user = User.create(
   name: 'nikola',
   email: 'user@test.com',
@@ -12,24 +7,27 @@ user = User.create(
 )
 
 20.times do
-  
-  giveaway =  user.giveaways.build(
+
+  giveaway =  user.giveaways.create(
     title: Faker::Commerce.product_name,
     description: Faker::Lorem.paragraphs,
     location: Faker::Address.city,
   )
-  # img = %w[lamp.png couch.png noguchi_table.jpg].sample
-  # giveaway.images.attach(io: File.open(Rails.root.join("app/assets/images/#{img}")), filename: "#{img}") 
 
-    rand(2..4).times do |c|
-      giveaway.comments.build(
-          body: Faker::Movies::Lebowski.quote,
-          user_id: 1
-        )
-    end
-  giveaway.save
+  img = %w[lamp.png couch.png noguchi_table.jpg].sample
+  pic = Picture.create(
+    title: img[0..-5],
+    giveaway_id: giveaway.id
+  )
+  pic.image.attach(io: File.open(Rails.root.join("app/assets/images/#{img}")), filename: "#{img}")
+
+  rand(2..4).times do |c|
+    giveaway.comments.create(
+        body: Faker::Movies::Lebowski.quote,
+        user_id: 1
+      )
+  end
+
 end
-
-
 
 puts 'Done!'
