766f9353e8a2c27fbf110890fb77b4ff009d5577
nikolatriki <kjofte@yahoo.com>
2021-10-28 00:06:23 +0200

    Removed migrations and added devise to users

---
+++ db/migrate/20211027215813_remove_emailand_password_from_users.rb
@@ -0,0 +1,6 @@
+class RemoveEmailandPasswordFromUsers < ActiveRecord::Migration[6.1]
+  def change
+    remove_column :users, :email, :string
+    remove_column :users, :password_digest, :string
+  end
+end
