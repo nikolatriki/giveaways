c3ab805afbdcee0820c44eabb4a9149df7c1720f
nikolatriki <kjofte@yahoo.com>
2021-10-30 00:05:22 +0200

    Refactor when User deleted

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :giveaways, dependent: :destroy
-  has_many :comments, dependent: :nullify
+  has_many :comments, dependent: :destroy
 
   validates :name, presence: true
   validates :email, email: { mode: :strict }
