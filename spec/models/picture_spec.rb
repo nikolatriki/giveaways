05833ddac13f64aecf20a76b2c9966c99216c021
nikolatriki <kjofte@yahoo.com>
2021-10-30 10:53:26 +0200

    Refactored Model Specs

--- spec/models/picture_spec.rb
+++ spec/models/picture_spec.rb
@@ -3,5 +3,18 @@
 require 'rails_helper'
 
 RSpec.describe Picture, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject(:picture) { create(:picture, giveaway: giveaway) }
+
+  let(:giveaway) { create(:giveaway, user: user) }
+  let(:user) { create(:user) }
+
+  describe 'validation' do
+    it { expect(picture).to validate_attachment(image: 'image/png') }
+  end
+
+  describe 'assosiations' do
+    it { expect(picture).to belong_to(:giveaway) }
+    it { expect(picture).to have_one_attached(:image) }
+  end
 end
+
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -9,11 +9,6 @@ RSpec.describe User, type: :model do
     it { expect(user).to validate_presence_of(:name) }
     it { expect(user).to validate_presence_of(:email) }
     it { expect(user).to validate_presence_of(:password).ignoring_interference_by_writer }
-    it { expect(user).to validate_uniqueness_of(:email).case_insensitive }
-    it { expect(user).to have_secure_password }
-    it { expect(user).to validate_length_of(:password).is_at_least(User::MINIMUM_PASSWORD_LENGTH) }
-    it { expect(user).to validate_length_of(:name).is_at_most(User::MAXIMUM_NAME_LENGTH) }
-    it { expect(user).to validate_length_of(:email).is_at_most(User::MAXIMUM_EMAIL_LENGTH) }
   end
 
   describe 'associations' do
@@ -23,7 +18,7 @@ RSpec.describe User, type: :model do
 
   describe 'dependency' do
     it { expect(user).to have_many(:giveaways).dependent(:destroy) }
-    it { expect(user).to have_many(:comments).dependent(:nullify) }
+    it { expect(user).to have_many(:comments).dependent(:destroy) }
   end
 
   context 'with email downcase' do
