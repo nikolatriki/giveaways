baa97f1bdff53b3c96bf7a4588f0263c5e9e220a
nikolatriki <kjofte@yahoo.com>
2021-11-05 23:02:29 +0100

    Refactored set_local method and added default url, added links to locales in layout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,10 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :set_locale
 
+  def default_url_options
+    { locale: I18n.locale }
+  end
+
   protected
 
   def configure_permitted_parameters
@@ -15,6 +19,7 @@ class ApplicationController < ActionController::Base
   private
 
   def set_locale
-    I18n.locale = params[:locale] || I18n.default_locale
+    locale = params[:locale].to_s.strip.to_sym
+    I18n.locale = I18n.available_locales.include?(locale) ? locale : I18n.default_locale
   end
 end
