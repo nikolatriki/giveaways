8489b64a347a83bcdcf30aace0b137523e8ffcb4
nikolatriki <kjofte@yahoo.com>
2021-11-06 22:41:40 +0100

    Localized time format, comments and giveaway forms and views

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,6 @@ class ApplicationController < ActionController::Base
   before_action :set_locale
   before_action { @pagy_locale = params[:locale] }
 
-
   def default_url_options
     { locale: (I18n.locale == I18n.default_locale ? nil : I18n.locale) }
   end
