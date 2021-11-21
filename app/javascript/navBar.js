3fd8584eb7d25d774754de058f83910290f002fd
nikolatriki <kjofte@yahoo.com>
2021-11-21 23:45:41 +0100

    Small refactor in views

--- app/javascript/navBar.js
+++ app/javascript/navBar.js
@@ -10,13 +10,12 @@ document.addEventListener('turbolinks:load', () => {
             el.addEventListener('click', () => {
 
              // Get the target from the "data-target" attribute
-             const target = el.dataset.target;
-             const $target = document.getElementById(target);
+            const target = el.dataset.target;
+            const $target = document.getElementById(target);
 
              // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
-             el.classList.toggle('is-active');
-             $target.classList.toggle('is-active');
-               
+            el.classList.toggle('is-active');
+            $target.classList.toggle('is-active');
             });
         });
     }
