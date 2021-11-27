e1dd4c70afd2a086c74d274f5b54d4d1ec45b2dc
nikolatriki <kjofte@yahoo.com>
2021-11-27 11:11:21 +0100

    Added view specs

---
+++ spec/views/dashboard/claims/index.html_spec.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+describe 'dashboard/claims/index.html.erb' do
+  let(:user) { create(:user) }
+  let(:giveaway) { create(:giveaway, user: user) }
+  let(:claim) { create(:claim, user: user, giveaway: giveaway) } 
+  
+
+  before do
+    assign(:user, user)
+    assign(:giveaways, [giveaway])
+    assign(:claims, [claim])
+  end
+
+  it 'displays the claimed giveaway card' do
+    render
+
+    expect(rendered).to include("#{claim.giveaway.title}")
+  end
+end
--- spec/views/dashboard/giveaways/new.html.erb_spec.rb
+++ spec/views/dashboard/giveaways/new.html.erb_spec.rb
@@ -3,24 +3,20 @@
 require 'rails_helper'
 
 describe 'dashboard/giveaways/new.html.erb' do
-  let(:heading) { double 'New Giveaway' }
+  let(:heading) { 'New Giveaway' }
+  let(:cancel_path) { dashboard_giveaways_path }
 
-  before do
-    allow(view).to receive(:heading) { heading }
-  end
-  
   it 'displays giveaways form' do
     assign(:giveaway, Giveaway.new)
-
-    render partial: 'form'
+    render partial: 'form', locals: { heading: heading, cancel_path: cancel_path }
 
     expect(rendered).to include('Title', 'Description', 'Location')
   end
 
   it 'displays error messages' do
     assign(:giveaway, Giveaway.create(title: '', description: '', location: ''))
-    
-    render partial: 'form'
+
+    render partial: 'form', locals: { heading: heading, cancel_path: cancel_path }
 
     expect(rendered).to include('Title can&#39;t be blank')
     expect(rendered).to include('Description can&#39;t be blank')
