4880fcbc48a88e7be70825191d85ddb883ed48d2
nikolatriki <kjofte@yahoo.com>
2021-11-27 18:19:08 +0100

    Failing test

--- spec/views/dashboard/giveaways/index.html.erb_spec.rb
+++ spec/views/dashboard/giveaways/index.html.erb_spec.rb
@@ -8,13 +8,19 @@ describe 'dashboard/giveaways/index.html.erb' do
   let(:claim) { create(:claim, user: user, giveaway: giveaway) }
 
   before do
-    assign(:user, user)
-    assign(:giveaways, [giveaway])
+    # assign(:user, user)
+    # assign(:giveaways, [giveaway])
+    assign(:waiting_response_my_giveaways,
+          [Claim.all.uniq(&:giveaway_id).map(&:giveaway).select do |giveaway|
+              giveaway.user_id == user.id
+            end])
+    # assign(:not_claimed_giveaways, [giveaways - waiting_response_my_giveaways])
   end
 
   it 'displays a giveaway card' do
     render
-
-    expect(rendered).to include(giveaway.title)
+    waiting_response_my_giveaways.each do |giveaway|
+      expect(rendered).to include(giveaway.title.truncate(20))
+    end
   end
 end
