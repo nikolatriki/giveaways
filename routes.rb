7c912dc407b6b19cfeeb9ea6f82753c037e9a56f
Nikola Trichkovski <60677366+nikolatriki@users.noreply.github.com>
2021-12-01 14:31:07 +0100

    Add files via upload

---
+++ routes.rb
@@ -0,0 +1,22 @@
+Rails.application.routes.draw do
+  devise_for :users, controllers: {
+    sessions: 'users/sessions',
+    registrations: 'users/registrations'
+  }
+  get 'users/:id', to: 'users#show', as: :user
+  
+  root 'giveaways#index'
+
+  resources :giveaways, only: %i[index show] do
+    resources :comments, :pictures
+    resources :claims, only: [:create]
+  end
+  namespace :dashboard do
+    resources :claims, only: [:index]
+    resources :giveaways
+    get 'pages', to: 'pages#home'
+    root 'pages#home'
+  end
+  
+
+end
