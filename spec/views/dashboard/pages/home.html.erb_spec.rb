be664949cddfb01591f2dd9e45b1b5f90b63670e
nikolatriki <kjofte@yahoo.com>
2021-12-09 15:20:44 +0100

    Refactore failing spec

--- spec/views/dashboard/pages/home.html.erb_spec.rb
+++ spec/views/dashboard/pages/home.html.erb_spec.rb
@@ -5,17 +5,20 @@ require 'rails_helper'
 describe 'dashboard/pages/home.html.erb', type: :view do
   let!(:user) { create :user }
   let!(:giveaway) { create(:giveaway, user: user) }
-  let!(:claim) { create(:claim) }
+  let!(:claim) { create(:claim, giveaway: giveaway, user: user) }
+  let(:given_giveaways) { [giveaway] }
+  let(:claims) { [claim] }
 
   before { sign_in user }
 
   it 'renders dashboard home' do
     assign(:user, user)
-    assign(:given_giveaways, [giveaway])
+    assign(:given_giveaways, given_giveaways)
     assign(:claims, [claim])
 
     render
-    expect(rendered).to include(given_giveaways)
-    expect(rendered).to include(claims)
+    expect(rendered).to include('Giveaways Given', given_giveaways.count.to_s)
+    expect(rendered).to include('My Giveaways', user.giveaways.count.to_s)
+    expect(rendered).to include('My Claims', user.claims.count.to_s)
   end
 end
