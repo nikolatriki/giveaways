fba9051061dde7f93dc09c9d0e8d1ebd1669239c
nikolatriki <kjofte@yahoo.com>
2021-12-11 23:50:29 +0100

    Generate Contact Model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_10_093054) do
+ActiveRecord::Schema.define(version: 2021_12_11_224343) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -61,6 +61,14 @@ ActiveRecord::Schema.define(version: 2021_12_10_093054) do
     t.bigint "user_id"
     t.index ["giveaway_id"], name: "index_comments_on_giveaway_id"
     t.index ["user_id"], name: "index_comments_on_user_id"
+  end
+
+  create_table "contacts", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.text "message"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "giveaways", force: :cascade do |t|
