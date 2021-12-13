bbc695ff6266e72044b49b1193b6a22482f9aed1
nikolatriki <kjofte@yahoo.com>
2021-12-13 11:41:30 +0100

    Install and config. gem letter_opener in development

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -76,4 +76,7 @@ Rails.application.configure do
 
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
+  #Use of email service gem letter opener
+  config.action_mailer.delivery_method = :letter_opener
+  config.action_mailer.perform_deliveries = true
 end
