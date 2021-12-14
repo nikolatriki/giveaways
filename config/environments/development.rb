f5b12d64d05503d146bf36b171cdf2006628f2ee
nikolatriki <kjofte@yahoo.com>
2021-12-14 16:38:34 +0100

    Removed letter_opener gem and config

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -76,7 +76,14 @@ Rails.application.configure do
 
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
-  #Use of gem letter opener
-  config.action_mailer.delivery_method = :letter_opener
-  config.action_mailer.perform_deliveries = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: "example.com",
+    authentication: "plain",
+    enable_starttls_auto: true,
+    user_name: ENV["GMAIL_USERNAME"],
+    password: ENV["GMAIL_PASSWORD"]    
+  }
 end
