0530f1ad981ebb57ed24f2110cb8da36a54294af
nikolatriki <kjofte@yahoo.com>
2021-12-15 21:44:02 +0100

    Added specs, fixed preview and rubocop fixes

--- spec/mailers/previews/approve_mailer_preview.rb
+++ spec/mailers/previews/approve_mailer_preview.rb
@@ -1,7 +1,10 @@
+# frozen_string_literal: true
+
 # Preview all emails at http://localhost:3000/rails/mailers/approve_mailer
 class ApproveMailerPreview < ActionMailer::Preview
   # Preview this email at http://localhost:3000/rails/mailers/approve_mailer/new_approve
   def new_approve
-    ApproveMailer.new_approve
+    giveaway = Giveaway.find_by(approved_to: !nil)
+    ApproveMailer.new_approve(giveaway)
   end
 end
