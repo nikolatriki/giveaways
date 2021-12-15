c2d4d3344ffc586cd67724ad9dd7968f6518e01d
nikolatriki <kjofte@yahoo.com>
2021-12-15 14:16:11 +0100

    Added specs and fixed preview mailer file

--- spec/mailers/claim_mailer_spec.rb
+++ spec/mailers/claim_mailer_spec.rb
@@ -1,18 +1,22 @@
-require "rails_helper"
+# frozen_string_literal: true
+
+require 'rails_helper'
 
 RSpec.describe ClaimMailer, type: :mailer do
-  describe "new_claim" do
-    let(:mail) { ClaimMailer.new_claim }
+  describe 'new_claim' do
+    let!(:user) { create(:user) }
+    let!(:giveaway) { create(:giveaway, user: user) }
+    let(:claim) { create(:claim, giveaway: giveaway) }
+    let(:mail) { described_class.new_claim(claim) }
 
-    it "renders the headers" do
-      expect(mail.subject).to eq("New claim")
-      expect(mail.to).to eq(["to@example.org"])
-      expect(mail.from).to eq(["from@example.com"])
+    it 'renders the headers' do
+      expect(mail.subject).to eq("New claim for #{giveaway.title}")
+      expect(mail.to).to eq([giveaway.user.email.to_s])
+      expect(mail.from).to eq(['support@giveaways.com'])
     end
 
-    it "renders the body" do
-      expect(mail.body.encoded).to match("Hi")
+    it 'renders the body' do
+      expect(mail.body.encoded).to include('A new claim')
     end
   end
-
 end
--- spec/mailers/previews/claim_mailer_preview.rb
+++ spec/mailers/previews/claim_mailer_preview.rb
@@ -3,7 +3,8 @@ class ClaimMailerPreview < ActionMailer::Preview
 
   # Preview this email at http://localhost:3000/rails/mailers/claim_mailer/new_claim
   def new_claim
-    ClaimMailer.new_claim
+    claim = Claim.last
+    ClaimMailer.new_claim(claim)
   end
 
 end
