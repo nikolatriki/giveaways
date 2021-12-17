148e03281bb63c23370e2fddf5e6d91a9b9a241f
nikolatriki <kjofte@yahoo.com>
2021-12-17 23:47:01 +0100

    Mailer production config gmail

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,7 +66,21 @@ Rails.application.configure do
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = false
+
+
+  config.action_mailer.default_url_options = { host: 'giveaways-staging.herokuapp.com'}
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: "gmail.com",
+    authentication: "plain",
+    enable_starttls_auto: true,
+    user_name: 'giveawaysheroku@gmail.com',
+    password: '!Giveaways123'    
+  }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
