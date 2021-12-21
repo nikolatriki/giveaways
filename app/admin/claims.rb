54921eec62e143b6608e0ebdfc3fdb9883c52582
nikolatriki <kjofte@yahoo.com>
2021-12-21 15:14:12 +0100

    Generate resources for models

---
+++ app/admin/claims.rb
@@ -0,0 +1,18 @@
+ActiveAdmin.register Claim do
+
+  # See permitted parameters documentation:
+  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+  #
+  # Uncomment all parameters which should be permitted for assignment
+  #
+  # permit_params :giveaway_id, :user_id
+  #
+  # or
+  #
+  # permit_params do
+  #   permitted = [:giveaway_id, :user_id]
+  #   permitted << :other if params[:action] == 'create' && current_user.admin?
+  #   permitted
+  # end
+  
+end
