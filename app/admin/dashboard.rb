8c306ae098657ae4e7891b16818c71dbaca96429
nikolatriki <kjofte@yahoo.com>
2021-12-22 00:09:00 +0100

    Added content to dashboard

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -2,31 +2,38 @@ ActiveAdmin.register_page "Dashboard" do
   menu priority: 1, label: proc { I18n.t("active_admin.dashboard") }
 
   content title: proc { I18n.t("active_admin.dashboard") } do
-    div class: "blank_slate_container", id: "dashboard_default_message" do
-      span class: "blank_slate" do
-        span I18n.t("active_admin.dashboard_welcome.welcome")
-        small I18n.t("active_admin.dashboard_welcome.call_to_action")
+    panel 'Recent messages to team' do
+      ul do
+        Contact.order(created_at: :desc).first(5).map do |c|
+          li link_to(c.message, admin_contact_path(c))
+        end
       end
     end
+    # div class: "blank_slate_container", id: "dashboard_default_message" do
+    #   span class: "blank_slate" do
+    #     span I18n.t("active_admin.dashboard_welcome.welcome")
+    #     small I18n.t("active_admin.dashboard_welcome.call_to_action")
+    #   end
+    # end
 
     # Here is an example of a simple dashboard with columns and panels.
     #
-    # columns do
-    #   column do
-    #     panel "Recent Posts" do
-    #       ul do
-    #         Post.recent(5).map do |post|
-    #           li link_to(post.title, admin_post_path(post))
-    #         end
-    #       end
-    #     end
-    #   end
+    columns do
+      column do
+        panel 'Recent Giveaways' do
+          ul do
+            Giveaway.order(created_at: :desc).first(5).map do |g|
+              li link_to(g.title, admin_giveaway_path(g))
+            end
+          end
+        end
+      end
 
-    #   column do
-    #     panel "Info" do
-    #       para "Welcome to ActiveAdmin."
-    #     end
-    #   end
-    # end
-  end # content
+      # column do
+      #   panel 'Info' do
+      #     para 'Welcome to ActiveAdmin.'
+      #   end
+      # end
+    end
+  end
 end
