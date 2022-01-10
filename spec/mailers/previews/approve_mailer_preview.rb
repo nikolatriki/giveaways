8ff5f6626b640499e822ffe5b9dbeb10c2dfb9f8
orcemaceski <orce@maceski.net>
2022-01-10 14:20:03 +0100

    Rubocop offense fix

--- spec/mailers/previews/approve_mailer_preview.rb
+++ spec/mailers/previews/approve_mailer_preview.rb
@@ -4,7 +4,7 @@
 class ApproveMailerPreview < ActionMailer::Preview
   # Preview this email at http://localhost:3000/rails/mailers/approve_mailer/new_approve
   def new_approve
-    giveaway = Giveaway.find_by(approved_to: !nil)
+    giveaway = Giveaway.where.not(approved_to: nil).first
     ApproveMailer.new_approve(giveaway)
   end
 end
